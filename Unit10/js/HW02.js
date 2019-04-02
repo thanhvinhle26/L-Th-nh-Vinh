2431291e0041fc21b6ddbd0dcb73179a54925e42
thanhvinhle26 <thanhvinhle26@gmail.com>
2019-04-02 18:09:17 +0700

    Bài11

--- Unit10/js/HW02.js
+++ Unit10/js/HW02.js
@@ -1,46 +1,58 @@
 function init(){
-		var tabs = document.getElementsByTagName("p");
-		for (var i = 0; i < tabs.length; i++) {
-			tabs[i].addEventListener("click",function(){
-				var active = document.getElementsByClassName("active")[0];
+	var tabs = document.getElementsByTagName("p");
+	var cha=document.getElementById('tabs');
+	
+	for (var i = 0; i < tabs.length; i++) {
+		tabs[i].addEventListener("click",function(){
+			var active = document.getElementsByClassName("active")[0];
 
-				active.className="tab";
-				this.parentNode.className="active tab";
-			});
+			active.className="tab";
+			this.parentNode.className="active tab";
+		});
+	}
+	var push=document.getElementsByTagName('i');
+
+	push[0].addEventListener("click",function(){
+		var active = document.getElementsByClassName("active")[0];
+		if(active.nextElementSibling.id!="icon")
+		{
+			active.nextElementSibling.className="active tab";
+			active.className="tab";
 		}
-		var push=document.getElementsByTagName('i');
-		push[0].addEventListener("click",function(){
-			var active = document.getElementsByClassName("active")[0];
-			if(active.nextElementSibling.id!="icon")
-			{
-				active.nextElementSibling.className="active tab";
-				active.className="tab";
-			}
-			
+		else
+		{
+			cha.children[1].className="active tab";
+			active.className="tab";
+		}
 
 
 
 
 
-		})
-		push[1].addEventListener("click",function(){
-			var active = document.getElementsByClassName("active")[0];
+	})
+	push[1].addEventListener("click",function(){
+		var active = document.getElementsByClassName("active")[0];
+		if(active.previousElementSibling.id!="icon")
+		{
 			active.previousElementSibling.className="active tab";
 			active.className="tab";
+		}
+		else
+		{
+			cha.children[cha.children.length-2].className="active tab";
+			active.className="tab";
+		}
+	})
+	
 
-
-
-
-
-		})
-		 var setTime=setInterval(function(){
-		 	var active = document.getElementsByClassName("active tab")[0];
-		 	if(active.nextElementSibling.id!="icon")
-		 	{active.nextElementSibling.className="active tab";
-		 	active.className="tab";
-		 }
-
-
-
-		 },5000);
+	var setTime=setInterval(function(){
+		var active = document.getElementsByClassName("active tab")[0];
+		if(active.nextElementSibling.id!="icon")
+			{active.nextElementSibling.className="active tab";
+		active.className="tab";
 	}
+
+
+
+},5000);
+}
